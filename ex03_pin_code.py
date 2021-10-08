9a779fe8661b909ab43316bf36522c9dc0d9778b
Vincent Michel <Vincent.michel@edu.devinci.Fr>
2021-10-08 18:51:29 +0200

    :white_check_mark: finished ex03
    .

--- ex03_pin_code.py
+++ ex03_pin_code.py
@@ -1,18 +1,16 @@
 import hashlib
-import datetime
 attempts = 0
-x = datetime.datetime.now()
-def test(attempts, x):
+def test(attempts):
     for i in range(10000000, 99999999):
         codeSearch = str(i)
         resultCodeSearch = hashlib.md5(codeSearch.encode())
-        OK = resultCodeSearch.hexdigest()
-        BOOMER = str(OK)
+        ResultDigested = resultCodeSearch.hexdigest()
+        FinishedResult = str(ResultDigested)
         attempts += 1
-        if BOOMER == "8a069869956a4e0cf7ac69f9c20e0d49":
-            return print(f"The code is : {codeSearch} and {attempts} attempts and {x.strftime('%S')}'s")
+        if FinishedResult == "8a069869956a4e0cf7ac69f9c20e0d49":
+            return print(f"The code is : {codeSearch} and {attempts} attempts")
 
-test(attempts, x)
+test(attempts)
 
 #password = 8a069869956a4e0cf7ac69f9c20e0d49
 
