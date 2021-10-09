d6c64cc77c8de56706a571f3aaed3add4aa97ba3
Vincent Michel <Vincent.michel@edu.devinci.Fr>
2021-10-09 16:29:10 +0200

    :white_check_mark: finished ex04_binary_to_hexa
    .

--- ex04_binary_to_hexa.py
+++ ex04_binary_to_hexa.py
@@ -1,1 +1,8 @@
-#hex function
+def binary_to_hexa(binary):
+    hexa = hex(int(binary, 2))
+    hexa2 = str(hexa)
+    return print(hexa2[2:])
+
+
+binary = str(input("put your binary code for traduce it in hexa"))
+binary_to_hexa(binary)
